1cd42b2b212c3c68d2aae0277882c53a894b0fec
sayyadshahid <shahidsayyed0212@gmail.com>
2025-09-28 20:18:41 +0530

    finally app setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Box>
+                    hey this is shahid
+                </Box>
+            </Router>
+        </>
+    )
+}
+
+export default App;
